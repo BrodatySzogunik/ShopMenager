d2c1e8b6f097626b5e04f13ec12d48da3530e7f3
Jakub <Jakub.mordalski2000@wp.pl>
2023-10-12 19:10:02 +0200

    git init

---
+++ src/main/java/ShopMenager/ShopMenager/ShopMenagerApplication.java
@@ -0,0 +1,13 @@
+package ShopMenager.ShopMenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMenagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ShopMenager/ShopMenager/ShopMenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ShopMenager.ShopMenager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMenagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
